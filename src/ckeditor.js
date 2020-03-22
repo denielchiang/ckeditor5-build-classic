c06d6ace7b6c233ed972499c76f3ba44ca085257
Deniel Chiang <deniel@jay-creative.com>
2020-03-22 18:08:46 +0800

    remove useless config

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -44,21 +44,6 @@ ClassicEditor.defaultConfig = {
 			'redo'
 		]
 	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
-	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'zh'
 };
